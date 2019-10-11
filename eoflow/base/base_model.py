7b7c05e29b3cb93e4d323806a14f66f1a402b33d
Lojze Zust <lojze.zust@sinergise.com>
2019-10-11 13:20:12 +0200

    Removed unused code in model

--- eoflow/base/base_model.py
+++ eoflow/base/base_model.py
@@ -19,28 +19,7 @@ class BaseModel(Configurable):
 
         self.summaries = []
 
-        # init the epoch counter
-        self.init_cur_epoch()
-
-    # save function that saves the checkpoint in the path defined in the config file
-    def save(self, sess):
-        self.saver.save(sess, self.config.checkpoint_dir, self.global_step_tensor)
-        logging.info("Model saved")
-
-    # load latest checkpoint from the experiment path defined in the config file
-    def load(self, sess):
-        latest_checkpoint = tf.train.latest_checkpoint(self.config.checkpoint_dir)
-        if latest_checkpoint:
-            self.saver.restore(sess, latest_checkpoint)
-            logging.info("Model loaded from checkpoint {}".format(latest_checkpoint))
-
-    # just initialize a tensorflow variable to use it as epoch counter
-    def init_cur_epoch(self):
-        with tf.variable_scope('cur_epoch'):
-            self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
-            self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
-
-    # just initialize a tensorflow variable to use it as global step counter
+    # Initialize a tensorflow variable to use it as global step counter
     def init_global_step(self):
         # DON'T forget to add the global step tensor to the tensorflow trainer
         with tf.variable_scope('global_step'):
@@ -93,11 +72,6 @@ class BaseModel(Configurable):
             return tf.summary.merge(self.summaries)
         else:
             return tf.constant("")
-
-    def init_saver(self):
-        # just copy the following line in your child class
-        # self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
-        raise NotImplementedError
 
     def build_model(self, features, labels, mode):
         """Builds the model for the provided input features and labels.
