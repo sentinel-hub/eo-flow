09cca22145de8a32b13458f44203059cacc29a44
Lojze Zust <lojze.zust@sinergise.com>
2019-10-16 09:46:40 +0200

    Removed old/unused code

--- eoflow/models/__init__.py
+++ eoflow/models/__init__.py
@@ -1,5 +1,4 @@
 from .example_model import ExampleModel
-from .template_model import TemplateModel
 from .fcn_model import FCNModel
 from .tfcn_model import TFCNModel
 from .rfcn_model import RFCNModel
--- eoflow/trainers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .example_trainer import ExampleTrainer
-from .template_trainer import TemplateTrainer
-from .fcn_trainer import FCNTrainer
