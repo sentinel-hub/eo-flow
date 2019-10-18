c486c294571bd9868d89b987d9e74cd397c3f93d
Lojze Zust <lojze.zust@sinergise.com>
2019-10-18 16:29:03 +0200

    Common shape util

--- eoflow/utils/utils.py
+++ eoflow/utils/utils.py
@@ -18,3 +18,14 @@ def create_dirs(dirs):
     except Exception as err:
         print("Creating directories error: {0}".format(err))
         exit(-1)
+
+def get_common_shape(shape1, shape2):
+    """ Get a common shape that fits both shapes. Dimensions that differ in size are set to None.
+        Example: [None, 20, 100, 50], [None, 20, 200, 50] -> [None, 20, None, 50]
+    """
+    if len(shape1) != len(shape2):
+        raise ValueError("Can't compute common shape. Ndims is different.")
+
+    common_shape = [dim1 if dim1==dim2 else None for dim1, dim2 in zip(shape1, shape2)]
+
+    return common_shape
