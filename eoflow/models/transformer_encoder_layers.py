79ff234903acc0b026a377d5417cb5cb343c004d
Lojze Zust <lojze.zust@sinergise.com>
2020-01-24 14:30:51 +0100

    TAE: Moved the layer normalization after encoding

--- eoflow/models/transformer_encoder_layers.py
+++ eoflow/models/transformer_encoder_layers.py
@@ -179,10 +179,10 @@ class Encoder(tf.keras.layers.Layer):
 
         x = self.dropout(x, training=training)
 
-        if self.lnorm_out:
-            x = self.lnorm_out(x)
-
         for i in range(self.num_layers):
             x = self.enc_layers[i](x, training, mask)
+
+        if self.lnorm_out:
+            x = self.lnorm_out(x)
 
         return x  # (batch_size, input_seq_len, d_model)
